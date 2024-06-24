cae2bc5b94472ea2789bb4171b79a8f04f6b1f07
Михаил Васильев <gnu.xinm@gmail.com>
2024-06-24 21:49:40 +0300

    added new hw

---
+++ 05-monitoring/evidently_metrics_calculation.py
@@ -0,0 +1,103 @@
+import datetime
+import time
+import random
+import logging 
+import uuid
+import pytz
+import pandas as pd
+import io
+import psycopg
+import joblib
+
+from prefect import task, flow
+
+from evidently.report import Report
+from evidently import ColumnMapping
+from evidently.metrics import ColumnDriftMetric, DatasetDriftMetric, DatasetMissingValuesMetric
+from evidently.metrics import ColumnQuantileMetric
+from evidently.metrics import DatasetCorrelationsMetric
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s]: %(message)s")
+
+SEND_TIMEOUT = 10
+rand = random.Random()
+
+create_table_statement = """
+drop table if exists dummy_metrics;
+create table dummy_metrics(
+	timestamp timestamp,
+	quantile float,
+	abs_max_features_correlation float
+)
+"""
+
+reference_data = pd.read_parquet('data/reference.parquet')
+with open('models/lin_reg.bin', 'rb') as f_in:
+	model = joblib.load(f_in)
+
+raw_data = pd.read_parquet('data/green_tripdata_2024-03.parquet')
+
+begin = datetime.datetime(2024, 3, 1, 0, 0)
+num_features = ['passenger_count', 'trip_distance', 'fare_amount', 'total_amount']
+cat_features = ['PULocationID', 'DOLocationID']
+column_mapping = ColumnMapping(
+    prediction='prediction',
+    numerical_features=num_features,
+    categorical_features=cat_features,
+    target=None
+)
+
+report = Report(metrics = [
+    DatasetCorrelationsMetric(),
+    ColumnQuantileMetric(quantile=0.5, column_name='fare_amount')
+])
+
+@task
+def prep_db():
+	with psycopg.connect("host=localhost port=5432 user=postgres password=example", autocommit=True) as conn:
+		res = conn.execute("SELECT 1 FROM pg_database WHERE datname='test'")
+		if len(res.fetchall()) == 0:
+			conn.execute("create database test;")
+		with psycopg.connect("host=localhost port=5432 dbname=test user=postgres password=example") as conn:
+			conn.execute(create_table_statement)
+
+@task
+def calculate_metrics_postgresql(curr, i):
+	current_data = raw_data[(raw_data.lpep_pickup_datetime >= (begin + datetime.timedelta(i))) &
+		(raw_data.lpep_pickup_datetime < (begin + datetime.timedelta(i + 1)))]
+
+	#current_data.fillna(0, inplace=True)
+	current_data['prediction'] = model.predict(current_data[num_features + cat_features].fillna(0))
+
+	report.run(reference_data = reference_data, current_data = current_data,
+		column_mapping=column_mapping)
+
+	result = report.as_dict()
+
+	quantile = result['metrics'][1]['result']['current']['value']
+	abs_max_features_correlation = result['metrics'][0]['result']['current']['stats']['pearson']['abs_max_features_correlation']
+
+	curr.execute(
+		"insert into dummy_metrics(timestamp, quantile, abs_max_features_correlation) values (%s, %s, %s)",
+		(begin + datetime.timedelta(i), quantile, abs_max_features_correlation)
+	)
+
+@flow
+def batch_monitoring_backfill():
+	prep_db()
+	last_send = datetime.datetime.now() - datetime.timedelta(seconds=10)
+	with psycopg.connect("host=localhost port=5432 dbname=test user=postgres password=example", autocommit=True) as conn:
+		for i in range(0, 27):
+			with conn.cursor() as curr:
+				calculate_metrics_postgresql(curr, i)
+
+			new_send = datetime.datetime.now()
+			seconds_elapsed = (new_send - last_send).total_seconds()
+			if seconds_elapsed < SEND_TIMEOUT:
+				time.sleep(SEND_TIMEOUT - seconds_elapsed)
+			while last_send < new_send:
+				last_send = last_send + datetime.timedelta(seconds=10)
+			logging.info("data sent")
+
+if __name__ == '__main__':
+	batch_monitoring_backfill()
